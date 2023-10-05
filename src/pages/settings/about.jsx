863f77972fb007a52a1b03d00df061c9eb2fc9f5
genesha <genesha.irzan@gmail.com>
2023-10-05 09:14:20 +0700

    gip rebuild

--- src/pages/settings/about.jsx
+++ src/pages/settings/about.jsx
@@ -1,11 +1,24 @@
 import UI from "@gh/ui";
+import github from "@img/icon/github2.png";
 
 export default function App(props) {
   return (
-    <UI.Stack>
+    <UI.Stack spacing={3}>
       <UI.Text variant="h4" bold color="primary">
         About
       </UI.Text>
+
+      <UI.Col
+        component="a"
+        href="https://github.com/geneshairzan/sampis-dashboard"
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{
+          width: 100,
+        }}
+      >
+        <img src={github} alt="" />
+      </UI.Col>
     </UI.Stack>
   );
 }
